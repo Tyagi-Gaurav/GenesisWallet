ee75a080683aaf1a9dc9f21d7b9ab161107eaa30
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2022-10-22 22:54:08 +0100

    44: Removed extra configuration of swagger

--- user/src/main/java/com/gw/user/config/SwaggerDocConfig.java
+++ user/src/main/java/com/gw/user/config/SwaggerDocConfig.java
@@ -3,8 +3,6 @@ package com.gw.user.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -12,7 +10,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @ConditionalOnExpression(value = "${useSwagger:true}")
-public class SwaggerDocConfig implements WebMvcConfigurer {
+public class SwaggerDocConfig {
 
     @Bean
     public Docket api() {
@@ -21,14 +19,5 @@ public class SwaggerDocConfig implements WebMvcConfigurer {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
--- user/src/main/java/com/gw/user/security/WebSecurityConfig.java
+++ user/src/main/java/com/gw/user/security/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig {
         return httpSecurity
                 .csrf().disable()
                 .authorizeExchange()
-                .pathMatchers("/status", "/actuator/**", "/swagger-ui.html", "/v2/**", "/user/**", "/swagger-ui/").permitAll()
+                .pathMatchers("/status", "/actuator/**", "/v2/**", "/user/**", "/swagger-ui/").permitAll()
                 .anyExchange().authenticated().and()
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
