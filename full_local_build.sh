433abab03321c65ebe371a774daf461bbc0b521f
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2023-03-15 10:52:02 +0000

    71: Fix functional tests

--- full_local_build.sh
+++ full_local_build.sh
@@ -2,8 +2,11 @@
 
 set -a
 
+docker-compose down --rmi all
+./gradlew clean build
+
 docker-compose --env-file ~/.secret/env.file up -d --build --remove-orphans
-WAIT_TIME=30
+WAIT_TIME=15
 echo "Waiting for ${WAIT_TIME} seconds for container to come up"
 sleep ${WAIT_TIME}
 
