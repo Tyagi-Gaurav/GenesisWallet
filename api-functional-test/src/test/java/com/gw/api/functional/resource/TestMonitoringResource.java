6549d7f4a0f9a2f9c24edbff1b594e9110bb4614
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2023-05-29 19:49:40 +0100

    65: Healthcheck for UI application

--- api-functional-test/src/test/java/com/gw/api/functional/resource/TestMonitoringResource.java
+++ api-functional-test/src/test/java/com/gw/api/functional/resource/TestMonitoringResource.java
@@ -23,7 +23,7 @@ public class TestMonitoringResource extends AbstractResource {
     public void accessUIStatus() {
         String fullUrl = getUrl(apiGatewayConfig.host().trim(),
                 apiGatewayConfig.uiContextPath(),
-                "/private/healthcheck/status", apiGatewayConfig.port());
+                "/private/health/status", apiGatewayConfig.port());
         responseHolder.setResponseWithBodyClass(this.get(fullUrl, EMPTY_HTTP_ENTITY, String.class), String.class);
     }
 
