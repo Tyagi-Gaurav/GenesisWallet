3bc468e2ebbe05e598c135f4485e39acd828f762
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2023-10-08 16:16:13 +0100

    1: Using grpc to connect from Node application

--- ui/app.js
+++ ui/app.js
@@ -3,11 +3,38 @@ import bodyParser from "body-parser";
 import protoLoader from "@grpc/proto-loader";
 import grpcLibrary from "@grpc/grpc-js";
 
+var PROTO_PATH = "../proto/UserService.proto";
+
 const app = express();
 const port = 3000; //TODO Read this from environment config
 
-const packageDefinition = protoLoader.loadSync(protoFileName, options);
-const packageObject = grpcLibrary.loadPackageDefinition(packageDefinition);
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true});
+
+console.log("Package Definition: " + packageDefinition);
+
+const userServiceObject = grpcLibrary.loadPackageDefinition(packageDefinition).com.gw.user.grpc;
+var userServiceClient = new userServiceObject.UserService('localhost:19090', grpcLibrary.credentials.createInsecure());
+
+console.log("Result: " + userServiceClient);
+
+userServiceClient.createUser({
+  userName : "testGrpc",
+  password : "abc",
+  firstName : "First",
+  lastName : "Last",
+  dateOfBirth : "15/10/2005"
+}, function(err, feature) {
+  if (err) {
+    console.log("Error occurred: " + err);
+  } else {
+    console.log("Feature : " + JSON.stringify(feature));
+  }
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
