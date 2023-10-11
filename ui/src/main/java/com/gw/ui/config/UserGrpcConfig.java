7224132987f10d1271f17c63991f58674016a088
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2023-10-11 13:30:15 +0100

    1: Delete Java UI for external user connector

--- ui/src/main/java/com/gw/ui/config/UserGrpcConfig.java
+++
@@ -1,8 +0,0 @@
-package com.gw.ui.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("user")
-public record UserGrpcConfig(String host, int port, long timeoutInMs,
-                             String circuitBreaker) {
-}
